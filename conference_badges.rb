bedc8a7edd396724c80264ce381fe94bfd0c0ec7
Alison Shin <alisonyshin@gmail.com>
2018-07-30 02:34:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ end
 
 def assign_rooms(names_list)
   room_hash = Hash.new
-  names_list.each.each_with_index{|name,room_number|
+  names_list.each_with_index{|name,room_number|
     room_hash[name] = room_number
     "Hello, #{name}! You'll be assigned to room #{room_number}!"
     
