cc24af2c6576e36f33bc5d80ceb66ee3803b8ef4
Alison Shin <alisonyshin@gmail.com>
2018-07-30 02:29:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,6 @@ def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
-  names_list.each {|name| badge_maker(name)}
+def batch_badge_creator(names_list)
+  badge_messages = names_list.collect {|name| badge_maker(name)}
 end
